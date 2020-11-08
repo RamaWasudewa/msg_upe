58cb6f2f1e7270756173878f1636a1ff617a65d0
RamaWasudewa <65174269+RamaWasudewa@users.noreply.github.com>
2020-11-08 22:03:34 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,12 @@
 from twilio.rest import Client 
  
 account_sid = 'AC912ee14df0e55c530e3abed534cc05bf' 
-auth_token = 'c532d872789201286873469768eeb20c' 
+auth_token = '386ad879c0f58871183916a5a80ee4f7' 
 client = Client(account_sid, auth_token) 
 def send_msg():
     message = client.messages.create( 
                               from_='whatsapp:+14155238886',  
-                              body='Hello Boy',      
+                              body='Your appointment is coming up on July 21 at 3PM',      
                               to='whatsapp:+628563893800' 
                           ) 
  
